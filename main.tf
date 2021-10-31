11de5a1380ae4430e6fb5580c4ae244a4ad32e90
KhRania <44244697+KhRania@users.noreply.github.com>
2021-10-31 09:58:14 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 
 # from where to allocate resources
 provider "kubernetes" {
-  config_path    = "~/.kube/config"
+  config_path    = "homme/runner/.kube/config"
   config_context = "kubernetes-admin@kubernetes"
 }
 
